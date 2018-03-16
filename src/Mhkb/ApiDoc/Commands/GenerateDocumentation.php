3d0e46c5ebd08ab95dae86364396989e2b774f20
Ivan <ivan@hennig.net.br>
2018-03-16 00:54:04 -0300

    Variables with camelCase style

--- src/Mhkb/ApiDoc/Commands/GenerateDocumentation.php
+++ src/Mhkb/ApiDoc/Commands/GenerateDocumentation.php
@@ -285,31 +285,31 @@ class GenerateDocumentation extends Command
         $routes = $this->getRoutes();
         $bindings = $this->getBindings();
         //Per route binding
-        $route_bindings = config('api.bindings');
-        $route_responses = [];
+        $routeBindings = config('api.bindings');
+        $routeResponses = [];
 
         $parsedRoutes = [];
         foreach ($routes as $route) {
-            $route_name = $route->getName();
+            $routeName = $route->getName();
 
-            if (in_array($route_name, $allowedRoutes) || str_is($routePrefix, $generator->getUri($route)) || in_array($middleware, $route->middleware())) {
+            if (in_array($routeName, $allowedRoutes) || str_is($routePrefix, $generator->getUri($route)) || in_array($middleware, $route->middleware())) {
                 $methods = $this->getMethods($route, $allowedMethods);
                 if ($this->isValidRoute($route) && $this->isRouteVisibleForDocumentation($route->getAction()['uses'])) {
-                    if ($route_bindings && !empty($route_bindings[$route_name])) {
-                        $current_bindings = $route_bindings[$route_name];
-                        foreach ($current_bindings as $key => $current_binding) {//Search for values like: RouteName@data.somevalue and replace for values of previous responses
-                            $current_bindings[$key] = preg_replace_callback("/{(\w+)@(.+)}/", function($m) use ($route_responses) {
-                                return $route_responses[$m[1]][$m[2]] ?? '';
-                            }, $current_binding);
+                    if ($routeBindings && !empty($routeBindings[$routeName])) {
+                        $currentBindings = $routeBindings[$routeName];
+                        foreach ($currentBindings as $key => $currentBinding) {//Search for values like: RouteName@data.somevalue and replace for values of previous responses
+                            $currentBindings[$key] = preg_replace_callback("/{(\w+)@(.+)}/", function($m) use ($routeResponses) {
+                                return $routeResponses[$m[1]][$m[2]] ?? '';
+                            }, $currentBinding);
                         }
                     } else {
-                        $current_bindings = $bindings;
+                        $currentBindings = $bindings;
                     }
-                    $current_header = $current_bindings['@header'] ?? $this->option('header');
-                    $parsed_route = $generator->processRoute($route, $current_bindings, $current_header, $withResponse, $methods, $this->option('locale'), $includeTags);
+                    $currentHeader = $currentBindings['@header'] ?? $this->option('header');
+                    $parsedRoute = $generator->processRoute($route, $currentBindings, $currentHeader, $withResponse, $methods, $this->option('locale'), $includeTags);
                     //Store the responses, might be used later
-                    $route_responses[$route_name] = array_dot(@json_decode($parsed_route['response'], true) ?: []);
-                    $parsedRoutes[] = $parsed_route;
+                    $routeResponses[$routeName] = array_dot(@json_decode($parsedRoute['response'], true) ?: []);
+                    $parsedRoutes[] = $parsedRoute;
                     $this->info('Processed route: ['.implode(',', $methods) .'] '.$generator->getUri($route));
                 } else {
                     $this->warn('Skipping route: ['.implode(',', $methods).'] '.$generator->getUri($route));
